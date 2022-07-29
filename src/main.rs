3eec61393abcd4ed494bcb707330071f9d9cee70
Azriel Hoh <azriel91@gmail.com>
2022-07-30 11:32:22 +1200

    Address clippy lints.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::{env, io};
 
 fn main() -> io::Result<()> {
-    // Skip 1 to skip executable path.
-    let file_path = env::args().skip(1).next();
+    // `nth(1)` skips executable path.
+    let file_path = env::args().nth(1);
     if let Some(file_path) = file_path {
         transact::process(file_path.as_ref(), io::stdout())
     } else {
